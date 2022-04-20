29fe00cb7e95e918bf22267f9d853335f361fb5b
SerraZ3 <serra.henrique3@gmail.com>
2022-04-19 21:49:01 -0300

    Importando e manipulando JSON

--- cinema.js
+++ cinema.js
@@ -1,1 +1,6 @@
-console.log("Criamos o arquivo");
+var catalogo = require("./database/catalogo.json");
+console.log(catalogo);
+var catalogoString = JSON.stringify(catalogo);
+console.log(catalogoString);
+var catalogoObj = JSON.parse(catalogoString);
+console.log(catalogoObj);
