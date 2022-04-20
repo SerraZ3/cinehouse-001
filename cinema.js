7a2dfa3576299e712f145778ddd51199e1cb7fb9
SerraZ3 <serra.henrique3@gmail.com>
2022-04-19 21:58:26 -0300

    Criado função para listar todos os filmes

--- cinema.js
+++ cinema.js
@@ -1,6 +1,13 @@
 var catalogo = require("./database/catalogo.json");
-console.log(catalogo);
+// console.log(catalogo);
 var catalogoString = JSON.stringify(catalogo);
-console.log(catalogoString);
+// console.log(catalogoString);
 var catalogoObj = JSON.parse(catalogoString);
-console.log(catalogoObj);
+// console.log(catalogoObj);
+
+function listarTodosOsFilmes(filmes) {
+  for (let i = 0; i < filmes.length; i++) {
+    console.log(filmes[i].titulo);
+  }
+}
+listarTodosOsFilmes(catalogoObj.data);
